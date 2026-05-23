ed007250ba6ecc4367d58181574f59f20decad36
Rafig <rafiqhaciyev2006@gmail.com>
2026-05-23 18:48:02 +0000

    Task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-import numpy as np
 import pandas as pd
 
 
 def from_numpy(array):
     """Creates a pd.DataFrame from a np.ndarray"""
-    import string
-    cols = list(string.ascii_uppercase[:array.shape[1]])
+    cols = [chr(65 + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=cols)
