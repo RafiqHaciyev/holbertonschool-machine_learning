7f52f9338a00aad49ebcb1d3e2d832f42ed55dfc
Rafig <rafiqhaciyev2006@gmail.com>
2026-05-23 19:01:50 +0000

    task 4

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for converting DataFrame columns to a numpy ndarray"""
+import pandas as pd
+
+
+def array(df):
+    """Selects last 10 rows of High and Close columns as a numpy ndarray"""
+    return df[['High', 'Close']].tail(10).to_numpy()
