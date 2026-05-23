b8f767b602e080c8cfa3370521031059b7494e86
Rafig <rafiqhaciyev2006@gmail.com>
2026-05-23 19:19:58 +0000

    doing tasks

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for creating a hierarchical pandas DataFrame"""
+import pandas as pd
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """Concatenates filtered DataFrames with Timestamp as first index level"""
+    df1 = index(df1)
+    df2 = index(df2)
+    df1 = df1[(df1.index >= 1417411980) & (df1.index <= 1417417980)]
+    df2 = df2[(df2.index >= 1417411980) & (df2.index <= 1417417980)]
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    df = df.swaplevel(0, 1).sort_index()
+    return df
