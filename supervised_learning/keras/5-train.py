98f9a6129c50b3900384b349f8db40e6756f1dc5
Rafig <rafiqhaciyev2006@gmail.com>
2026-07-18 19:43:57 +0000

    Tensorflow and keras

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -4,8 +4,9 @@ gradient descent, with optional validation data."""
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,
-                 validation_data=None, verbose=True, shuffle=False):
+def train_model(
+        network, data, labels, batch_size, epochs,
+        validation_data=None, verbose=True, shuffle=False):
     """Train a model using mini-batch gradient descent, optionally
     analyzing validation data.
 
@@ -30,12 +31,13 @@ def train_model(network, data, labels, batch_size, epochs,
         History: the History object generated after training the
         model.
     """
-    history = network.fit(x=data,
-                           y=labels,
-                           batch_size=batch_size,
-                           epochs=epochs,
-                           validation_data=validation_data,
-                           verbose=verbose,
-                           shuffle=shuffle)
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        verbose=verbose,
+        shuffle=shuffle)
 
     return history
